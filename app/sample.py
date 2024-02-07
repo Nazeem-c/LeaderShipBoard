a491891d4aed8cd2b7aeb615e95bde6888ba35b2
ShesnaJalal <shesnajalal211@gmail.com>
2024-02-07 10:25:03 +0530

    4 th commit

--- app/sample.py
+++ app/sample.py
@@ -663,7 +663,7 @@ def add_student():
                 login_id
             ))
  
-            # Commit changes and close 
+            # Commit changes and close connection ....
             connection.commit()
             cursor.close()
             connection.close()
