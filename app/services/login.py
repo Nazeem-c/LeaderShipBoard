e1a9067985b2da02a5eabe4b1ad35deb2c7d3efc
Nazeem-c <mhdnazeemc@gmail.com>
2024-02-20 11:06:13 +0530

    Leaderboard table page is done

--- app/services/login.py
+++ app/services/login.py
@@ -10,6 +10,37 @@ app.config['SECRET_KEY'] = 'leadershipboard'
 
 
 
+# def login():
+#     data = request.get_json()
+#     username = data.get('username')
+#     password = data.get('password')
+
+#     query = "SELECT login_id, username, roll FROM login WHERE username = %s AND password = %s"
+
+#     try:
+#         with psycopg2.connect(**db_params) as conn:
+#             with conn.cursor() as cur:
+#                 cur.execute(query, (username, password))
+#                 user = cur.fetchone()
+
+#         if user:
+#             session['login_id'] = user[0]
+#             session['username'] = user[1]
+#             session['roll'] = user[2]
+
+#             if user[2] == 'admin':
+#                 return redirect(url_for('admin.adminpage'))
+#             elif user[2] == 'student':
+#                 return redirect(url_for('students.studentpage'))
+#             else:
+#                 return generate_response({'error': 'Invalid role'}, 401)
+#         else:
+#              generate_response({'error': 'Invalid credentials'}, 401)
+
+#     except Exception as e:
+#         return generate_response({'error': str(e)}, 500)
+
+
 def login():
     data = request.get_json()
     username = data.get('username')
@@ -29,17 +60,26 @@ def login():
             session['roll'] = user[2]
 
             if user[2] == 'admin':
-                return redirect(url_for('admin.adminpage'))
+                return jsonify({
+                    'user_id': user[0],
+                    'username': user[1],
+                    'roll': user[2],
+                    'redirect_url': url_for('admin.adminpage')
+                })
             elif user[2] == 'student':
-                return redirect(url_for('students.studentpage'))
+                return jsonify({
+                    'user_id': user[0],
+                    'username': user[1],
+                    'roll': user[2],
+                    'redirect_url': url_for('students.studentpage')
+                })
             else:
                 return generate_response({'error': 'Invalid role'}, 401)
         else:
-             generate_response({'error': 'Invalid credentials'}, 401)
+            return generate_response({'error': 'Invalid credentials'}, 401)
 
     except Exception as e:
         return generate_response({'error': str(e)}, 500)
-
 
 def logout():
     session.pop('login_id', None)
